0663a9b63e8169e7a0b5b58bd1bd88c8448021c3
joaopsilva <joao.pcg.silva@gmail.com>
2017-10-20 22:56:39 +0200

    Update depositList property name after API update.

--- src/main/java/com/binance/api/client/domain/account/DepositHistory.java
+++ src/main/java/com/binance/api/client/domain/account/DepositHistory.java
@@ -13,7 +13,7 @@ import java.util.List;
  */
 public class DepositHistory {
 
-  @JsonProperty("withdrawList")
+  @JsonProperty("depositList")
   private List<Deposit> depositList;
 
   private boolean success;
