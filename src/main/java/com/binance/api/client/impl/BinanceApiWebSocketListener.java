a07a578bd2817e378e0120a4197eadd928ba6110
Amirul Zin <amirul.mdzin@gmail.com>
2018-01-13 18:53:39 +0800

    Fix ObjectMapper not being cached per response call

--- src/main/java/com/binance/api/client/impl/BinanceApiWebSocketListener.java
+++ src/main/java/com/binance/api/client/impl/BinanceApiWebSocketListener.java
@@ -18,14 +18,20 @@ public class BinanceApiWebSocketListener<T> extends WebSocketListener {
 
   private Class<T> eventClass;
 
+  private ObjectMapper mapper;
+
   public BinanceApiWebSocketListener(BinanceApiCallback<T> callback, Class<T> eventClass) {
+    this(callback, eventClass, new ObjectMapper());
+  }
+
+  public BinanceApiWebSocketListener(BinanceApiCallback<T> callback, Class<T> eventClass, ObjectMapper mapper) {
     this.callback = callback;
     this.eventClass = eventClass;
+    this.mapper = mapper;
   }
 
   @Override
   public void onMessage(WebSocket webSocket, String text) {
-    ObjectMapper mapper = new ObjectMapper();
     try {
       T event = mapper.readValue(text, eventClass);
       callback.onResponse(event);
