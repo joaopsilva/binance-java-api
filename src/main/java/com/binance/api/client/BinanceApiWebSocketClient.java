a10dc0c0ab7fd848381af677f93cbf678c831a4e
Andy Coates <andy.coates@apple.com>
2018-02-04 15:00:57 +0000

    -Removed unused imports -Added Java docs to make it clear you can close the websocket by calling close on the returned `Closeable`

--- src/main/java/com/binance/api/client/BinanceApiWebSocketClient.java
+++ src/main/java/com/binance/api/client/BinanceApiWebSocketClient.java
@@ -1,6 +1,5 @@
 package com.binance.api.client;
 
-import java.util.List;
 import com.binance.api.client.domain.event.AggTradeEvent;
 import com.binance.api.client.domain.event.AllMarketTickersEvent;
 import com.binance.api.client.domain.event.CandlestickEvent;
@@ -9,21 +8,57 @@ import com.binance.api.client.domain.event.UserDataUpdateEvent;
 import com.binance.api.client.domain.market.CandlestickInterval;
 
 import java.io.Closeable;
+import java.util.List;
 
 /**
  * Binance API data streaming façade, supporting streaming of events through web sockets.
  */
 public interface BinanceApiWebSocketClient extends Closeable {
 
-  Closeable onDepthEvent(String symbol, BinanceApiCallback<DepthEvent> callback);
+    /**
+     * Open a new web socket to receive {@link DepthEvent depthEvents} on a callback.
+     *
+     * @param symbol   the market symbol to subscribe to
+     * @param callback the callback to call on new events
+     * @return a {@link Closeable} that allows the underlying web socket to be closed.
+     */
+    Closeable onDepthEvent(String symbol, BinanceApiCallback<DepthEvent> callback);
 
-  Closeable onCandlestickEvent(String symbol, CandlestickInterval interval, BinanceApiCallback<CandlestickEvent> callback);
+    /**
+     * Open a new web socket to receive {@link CandlestickEvent candlestickEvents} on a callback.
+     *
+     * @param symbol   the market symbol to subscribe to
+     * @param interval the interval of the candles tick events required
+     * @param callback the callback to call on new events
+     * @return a {@link Closeable} that allows the underlying web socket to be closed.
+     */
+    Closeable onCandlestickEvent(String symbol, CandlestickInterval interval, BinanceApiCallback<CandlestickEvent> callback);
 
-  Closeable onAggTradeEvent(String symbol, BinanceApiCallback<AggTradeEvent> callback);
+    /**
+     * Open a new web socket to receive {@link AggTradeEvent aggTradeEvents} on a callback.
+     *
+     * @param symbol   the market symbol to subscribe to
+     * @param callback the callback to call on new events
+     * @return a {@link Closeable} that allows the underlying web socket to be closed.
+     */
+    Closeable onAggTradeEvent(String symbol, BinanceApiCallback<AggTradeEvent> callback);
 
-  Closeable onUserDataUpdateEvent(String listenKey, BinanceApiCallback<UserDataUpdateEvent> callback);
+    /**
+     * Open a new web socket to receive {@link UserDataUpdateEvent userDataUpdateEvents} on a callback.
+     *
+     * @param listenKey the listen key to subscribe to.
+     * @param callback  the callback to call on new events
+     * @return a {@link Closeable} that allows the underlying web socket to be closed.
+     */
+    Closeable onUserDataUpdateEvent(String listenKey, BinanceApiCallback<UserDataUpdateEvent> callback);
 
-  Closeable onAllMarketTickersEvent(BinanceApiCallback<List<AllMarketTickersEvent>> callback);
-  
-  void close();
+    /**
+     * Open a new web socket to receive {@link AllMarketTickersEvent allMarketTickersEvents} on a callback.
+     *
+     * @param callback the callback to call on new events
+     * @return a {@link Closeable} that allows the underlying web socket to be closed.
+     */
+    Closeable onAllMarketTickersEvent(BinanceApiCallback<List<AllMarketTickersEvent>> callback);
+
+    void close();
 }
