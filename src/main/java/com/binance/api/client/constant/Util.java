e97c006c5cfcaeb0e07590d04efc1472b72fc8d8
Oleg Yudin <puzatin@gmail.com>
2019-12-20 17:16:58 +0700

    add turkish lira (TRY)

--- src/main/java/com/binance/api/client/constant/Util.java
+++ src/main/java/com/binance/api/client/constant/Util.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public final class Util {
 
-    public static final List<String> FIAT_CURRENCY = Collections.unmodifiableList(Arrays.asList("USDT", "BUSD", "PAX", "TUSD", "USDC", "NGN", "RUB", "USDS"));
+    public static final List<String> FIAT_CURRENCY = Collections.unmodifiableList(Arrays.asList("USDT", "BUSD", "PAX", "TUSD", "USDC", "NGN", "RUB", "USDS, TRY"));
 
     public static final String BTC_TICKER = "BTC";
 
