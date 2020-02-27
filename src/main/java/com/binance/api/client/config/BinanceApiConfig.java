3a8dbff60742e718fb20e5981d4d1230142c6724
Dave Mc <dave@davemx.com>
2020-02-26 17:56:50 -0700

    Allow the base domain to be customized so that binance.us can be used instead of binance.com.

---
+++ src/main/java/com/binance/api/client/config/BinanceApiConfig.java
@@ -0,0 +1,57 @@
+package com.binance.api.client.config;
+
+/**
+ * Configuration used for Binance operations.
+ */
+public class BinanceApiConfig {
+
+	/**
+	 * Base domain for URLs.
+	 */
+	private static String BASE_DOMAIN = "binance.com";
+
+	/**
+	 * Default receiving window.
+	 */
+	private static long DEFAULT_RECEIVING_WINDOW = 6_000_000L;
+
+	/**
+	 * Set the URL base domain name (e.g., binance.com).
+	 *
+	 * @param baseDomain Base domain name
+	 */
+	public static void setBaseDomain(final String baseDomain) {
+		BASE_DOMAIN = baseDomain;
+	}
+
+	/**
+	 * Get the URL base domain name (e.g., binance.com).
+	 *
+	 * @return The base domain for URLs
+	 */
+	public static String getBaseDomain() {
+		return BASE_DOMAIN;
+	}
+
+	/**
+	 * REST API base URL.
+	 */
+	public static String getApiBaseUrl() {
+		return String.format("https://api.%s", getBaseDomain());
+	}
+
+	/**
+	 * Streaming API base URL.
+	 */
+	public static String getStreamApiBaseUrl() {
+		return String.format("wss://stream.%s:9443/ws", getBaseDomain());
+	}
+
+	/**
+	 * Asset info base URL.
+	 */
+	public static String getAssetInfoApiBaseUrl() {
+		return String.format("https://%s/", getBaseDomain());
+	}
+
+}
